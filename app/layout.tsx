b51490932bc24291cbff8e7d37cf1ca81cb03588
curiousgeorgios <digitalnachos@proton.me>
2025-08-08 15:43:41 +0300

    fix: meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import MetaPixel from "../components/MetaPixel"
 
 const inter = Inter({ subsets: ["latin"], weight: ["100", "200", "300", "400", "500"] })
 
@@ -21,36 +22,14 @@ export default function RootLayout({
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
       <link rel="manifest" href="/site.webmanifest"></link>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1813818262504729');
-            fbq('track', 'PageView');
-          `,
-        }}
-      />
-      <noscript>
-        <img 
-          height="1" 
-          width="1" 
-          style={{display: 'none'}}
-          src="https://www.facebook.com/tr?id=1813818262504729&ev=PageView&noscript=1"
-          alt=""
-        />
-      </noscript>
-      <body className={inter.className}>{children}</body>
+      
+      
+      <body className={inter.className}>
+        <MetaPixel />
+        {children}
+      </body>
     </html>
   )
 }
 
 
-
-import './globals.css'
